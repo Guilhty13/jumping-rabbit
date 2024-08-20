f1efe36d0357f353f51ac7cf9b67d5a6e4542c81
Guilherme Evangelista Malveira <Guilherme50500336@edu.df.senac.br>
2024-08-20 18:21:54 -0300

    Botei menu

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    public string sceneToLoad; // Nome da cena para carregar quando o jogador entra na porta
+    public string Final; // Nome da cena para carregar quando o jogador entra na porta
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) // Verifica se o objeto que colidiu Ã© o jogador
         {
-            SceneManager.LoadScene(sceneToLoad); // Carrega a cena especificada
+            SceneManager.LoadScene(Final); // Carrega a cena especificada
         }
     }
 }
